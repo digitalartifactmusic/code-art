5363be9d048c1d201880d08db2b75ffc72824c81
Noah Grassmeyer <54650033+ngrassmeyer@users.noreply.github.com>
2021-02-16 12:18:04 -0600

    Update DLA.cpp

--- Diffusion Limited Aggregation/DLA.cpp
+++ Diffusion Limited Aggregation/DLA.cpp
@@ -1,78 +1,215 @@
-#include "DLA.h"
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+#include <time.h>
+#include <vector>
+#include <algorithm>
+
+#include <list>
+
+//#include <thread>
+
+static unsigned NUM_SEEDS = 15, NUM_MOVERS = 300000, ADDMOVERS = 10, GRID_H = 1000, TOPINDEX_H = GRID_H - 1, GRID_W = 1000, TOPINDEX_W = GRID_W - 1;
+
+using namespace std;
+
+//thread T1, T2, T3;
+
+static vector<unsigned> RANDOM(NUM_MOVERS * 2);
+
+static vector<unsigned>::iterator RANDOM_ITR = RANDOM.begin();
+
+static const unsigned SIZE = GRID_W * GRID_H;
+sf::VertexArray VERTEXARRAY(sf::Points, SIZE);
+
+const uint64_t SEED = time(0);
+
+uint64_t _seed{ SEED }; /* The state can be seeded with any value. */
+uint64_t _next()
+{
+	uint64_t z = (_seed += UINT64_C(0x9E3779B97F4A7C15));	// <- Not my code.
+	z = (z ^ (z >> 30))* UINT64_C(0xBF58476D1CE4E5B9);
+	z = (z ^ (z >> 27))* UINT64_C(0x94D049BB133111EB);
+	return z ^ (z >> 31);
+}
+
+sf::Color COLORS[100000];
+sf::Color* COLORSBEGIN = COLORS;
+sf::Color* COLORSITR = COLORS;
+
+class DLA
+{
+
+private:
+
+	unsigned _x, _y, _xMove, _yMove;
+
+	list<pair<unsigned, unsigned>> _movers;
+	list<pair<unsigned, unsigned>>::iterator _index;
+
+	vector<vector<bool>> _grid;
+
+	vector<vector<sf::Color*>> _aggregated;
+
+	vector<pair<unsigned, unsigned>> _anim;
+
+	sf::Color* _targetColor;
+
+	vector<vector<pair<unsigned, unsigned>>> _empty;
+	vector<vector<unsigned>> _emptyX;		// Maps to _empty, keeps indices static during erasures.
+	vector<unsigned> _emptyY;				// x vector allows x deletes and y vector allows y deletes.
+
+	size_t _numMovers, _numSeeds;
+
+public:
+
+	DLA() = delete;
+
+	DLA(unsigned numMovers, unsigned numSeeds);
+
+	void iterateClassic();
+
+	void draw(unsigned x, unsigned y)
+	{
+		_grid[y][x] = 1;
+		*_aggregated[y][x] = { 160 , 60 , 230 };
+	}
+
+	unsigned getMovers() { return _movers.size(); };
+
+	void animateClassic();
+
+	void makeSeed(const unsigned num, const bool grav);
+	void makeMover(const unsigned num);
+
+	void erase(const unsigned x, const unsigned y);
+
+};
 
 DLA::DLA(unsigned numMovers, unsigned numSeeds) : _numMovers{ numMovers }, _numSeeds{ numSeeds }
 {
 	for (unsigned i = 0; i < GRID_H; i++)
 	{
-		_empty.push_back(vector<Point>());
+		_empty.push_back(vector<pair<unsigned, unsigned>>());
 		_emptyX.push_back(vector<unsigned>());
 		_emptyY.push_back(i);
 		for (unsigned j = 0; j < GRID_W; j++)
 		{
-			_empty[i].push_back(Point{ j , i });
+			_empty[i].push_back({ j , i });
 			_emptyX[i].push_back(j);
 		}
 	}
-	_aggregated.resize(GRID_H, vector<Color>(GRID_W, Color{ 0 , 0 , 0 , 0 }));
+	_aggregated.resize(GRID_H, vector<sf::Color*>(GRID_W, COLORSBEGIN));
 	_grid.resize(GRID_H, vector<bool>(GRID_W, 0));
-	_movers.reserve(GRID_W* GRID_H);
 	makeSeed(_numSeeds, 0);
 	makeMover(_numMovers);
+
+	COLORSITR = COLORSBEGIN;
+	sf::Uint8 r = 160, g = 60, b = 230, pos = 2;
+
+	//for (unsigned i = 1; i < 10000 + 1; i++)
+	//{
+	//	r = 0, g = 0, b = 0;
+	//	if ((i / 255) % 2)
+	//	{
+	//		r = 255 - (i % 256);
+	//		b = 255;
+	//	}
+	//	else
+	//	{
+	//		for (unsigned j = 0; j < i; j++)
+	//		{
+	//			r += 9;
+	//			if (r > 255)
+	//			{
+	//				r = 0;
+	//				b += 11;
+	//				if (b > 255)
+	//				{
+	//					b = 0;
+	//				}
+	//			}
+	//		}
+	//		//r = (i % 256);
+	//		//b = r;
+	//	}
+
+	//	COLORS[i] = sf::Color{ r, g, b };
+	//}
+
+	for (unsigned i = 0; i < 100000; i++)
+	{
+		switch (pos)
+		{
+		case 0:
+
+			if (g != 255)
+				g++;
+			else if (r != 0)
+				r--;
+			else
+				pos++;
+
+			break;
+		case 1:
+
+			if (b != 255)
+				b++;
+			else if (g != 0)
+				g--;
+			else
+				pos++;
+
+			break;
+		case 2:
+
+			if (r != 255)
+				r++;
+			else if (b != 0)
+				b--;
+			else
+				pos = 0;
+
+			break;
+		}
+		*COLORSITR = sf::Color{ r, g, b };
+		COLORSITR++;
+	}
+
+	COLORSITR = COLORSBEGIN;
 }
 
 void DLA::animateClassic()
 {
-	for (unsigned k = 0; k < 100; k++)
-	{
-		for (unsigned i = 0; i < _anim.size(); i++)
-		{
-			for (unsigned j = 0; j < _anim[i].size(); j++)
-			{
-				_y = _anim[i][j].getY();
-				_x = _anim[i][j].getX();
-				_targetColor = _aggregated[_y].begin() + _x;
-				_targetColor->advanceSecondary(144);
-				SetPixel(_deviceContext, _x, _y, RGB(_targetColor->getR(), _targetColor->getG(), _targetColor->getB()));
-			}
-		}
-	}
+	for (auto& i : _anim)
+		VERTEXARRAY[(i.second * GRID_H) + i.first].color = *(++_aggregated[i.second][i.first]);
 }
 
 void DLA::makeMover(const unsigned num)
 {
 	if (_empty.size())
-	{
 		for (unsigned i = 0; i < num; i++)
-		{
-			_y = _next() % _empty.size();
-			_x = _next() % _empty[_y].size();
-
-			_movers.push_back(Point{ _empty[_y][_x].getX() , _empty[_y][_x].getY() });
-		}
-	}
+			_movers.push_back(_empty[_next() % _empty.size()][_x = _next() % _empty[_y].size()]);
 }
 
 void DLA::makeSeed(const unsigned num, const bool grav)
 {
 	for (unsigned i = 0; i < num; i++)
 	{
-		_y = _next()% _empty.size();
-		_x = _next()% _empty[_y].size();
+		_y = _next() % _empty.size();
+		_x = _next() % _empty[_y].size();
 
 		if (grav)
 			_y = GRID_H - 1;
 
 		_grid[_y][_x] = 1;
-		_aggregated[_y][_x] = Color{ i , 160 , 60 , 220 };
-		SetPixel(_deviceContext, _x, _y, RGB(_aggregated[_y][_x].getR(), _aggregated[_y][_x].getG(), _aggregated[_y][_x].getB()));
+		*_aggregated[_y][_x] = sf::Color{ 160 , 60 , 230 };
+		VERTEXARRAY[(_y * GRID_H) + _x].color = sf::Color{ _aggregated[_y][_x]->r, _aggregated[_y][_x]->g, _aggregated[_y][_x]->b };
 
 		erase(_x, _y);
 
-		_anim.push_back(vector<Point>());
-		_anim[i].push_back( Point{ _x , _y } );
+		_anim.push_back(pair<unsigned, unsigned>{ _x, _y });
 	}
-
-
 }
 
 void DLA::erase(const unsigned x, const unsigned y)
@@ -95,189 +232,259 @@ void DLA::erase(const unsigned x, const unsigned y)
 	}
 }
 
-DLA::~DLA()
+void DLA::iterateClassic()
 {
-	ReleaseDC(_consoleHandle, _deviceContext);
-}
-
-void DLA::iterateClassicFill()	// Not adding any movers. Not checking mover space aggregation. Not updating number of movers each iteration. Not preparing erased vector for random access.
-{
-	makeMover(ADDMOVERS);
 	_index = _movers.begin();
 	_numMovers = _movers.size();
 
 	for (unsigned i = 0; i < _numMovers; i++)
 	{
-		switch (i % 4)		// This is a rough approximation. Generating random numbers is hard The movers are randomly distributed accross the board though, so it mimicks a random walk fairly well.
+		_x = _index->first;
+		_y = _index->second;
+		switch (*(++RANDOM_ITR))
 		{
 		case 0:
 		{
-			_x = _index->getX();
 
 			if (_x == TOPINDEX_W)
 			{
-				_index++;
-				break;
+				if (_grid[_y][0])
+				{
+					_grid[_y][_x] = 1;
+					_index = _movers.erase(_index);
+					--_numMovers;
+
+					_aggregated[_y][_x] = _aggregated[_y][0] + 1;
+					VERTEXARRAY[(_y * GRID_H) + _x].color = *_aggregated[_y][_x];
+					_anim.push_back({ _x, _y });
+					break;
+				}
+				else
+				{
+					_index->first = 0;
+				}
 			}
-
-			_y = _index->getY();
-
-			if (_grid[_y][_x])
+			else
 			{
-				_index = _movers.erase(_index);
-				_numMovers--;
+				if (_grid[_y][_x + 1])
+				{
+					_grid[_y][_x] = 1;
+					_index = _movers.erase(_index);
+					--_numMovers;
 
-				break;
+					_aggregated[_y][_x] = _aggregated[_y][_x + 1] + 1;
+					VERTEXARRAY[(_y * GRID_H) + _x].color = *_aggregated[_y][_x];
+					_anim.push_back({ _x, _y });
+					break;
+				}
+				else
+				{
+					++(_index->first);
+				}
 			}
 
-			if (_grid[_y][_x + 1])
-			{
-				_grid[_y][_x] = 1;
-				_index = _movers.erase(_index);
-				_numMovers--;
-
-				_targetColor = _aggregated[_y].begin() + _x;
-				_anim[_targetColor->getRegion()].push_back(Point{ _x , _y });
-				*_targetColor = *(_targetColor + 1);
-				_targetColor->advanceSecondary();
-				erase(_x, _y);
-				SetPixel(_deviceContext, _x, _y, RGB(_targetColor->getR(), _targetColor->getG(), _targetColor->getB()));
-
-				break;
-			}
-
-			_index->upX();
-			_index++;
+			++_index;
 
 			break;
 		}
-
 		case 1:
 		{
-			_x = _index->getX();
-
-			if (!_x)
+			if (_x == 0)
 			{
-				_index++;
-				break;
+				if (_grid[_y][TOPINDEX_W])
+				{
+					_grid[_y][_x] = 1;
+					_index = _movers.erase(_index);
+					--_numMovers;
+
+					_aggregated[_y][_x] = _aggregated[_y][TOPINDEX_W] + 1;
+					VERTEXARRAY[(_y * GRID_H) + _x].color = *_aggregated[_y][_x];
+					_anim.push_back({ _x, _y });
+					break;
+				}
+				else
+				{
+					_index->first = TOPINDEX_W;
+				}
+			}
+			else
+			{
+				if (_grid[_y][_x - 1])
+				{
+					_grid[_y][_x] = 1;
+					_index = _movers.erase(_index);
+					--_numMovers;
+
+					_aggregated[_y][_x] = _aggregated[_y][_x - 1] + 1;
+					VERTEXARRAY[(_y * GRID_H) + _x].color = *_aggregated[_y][_x];
+					_anim.push_back({ _x, _y });
+					break;
+				}
+				else
+				{
+					--(_index->first);
+				}
 			}
 
-			_y = _index->getY();
-
-			if (_grid[_y][_x])
-			{
-				_index = _movers.erase(_index);
-				_numMovers--;
-
-				break;
-			}
-
-			if (_grid[_y][_x - 1])
-			{
-				_grid[_y][_x] = 1;
-				_movers.erase(_index);
-				_numMovers--;
-
-				_targetColor = _aggregated[_y].begin() + _x;
-				_anim[_targetColor->getRegion()].push_back(Point{ _x , _y });
-				*_targetColor = *(_targetColor - 1);
-				_targetColor->advanceSecondary();
-				erase(_x, _y);
-				SetPixel(_deviceContext, _x, _y, RGB(_targetColor->getR(), _targetColor->getG(), _targetColor->getB()));
-
-				break;
-			}
-
-			_index->downX();
-			_index++;
+			++_index;
 
 			break;
 		}
 		case 2:
 		{
-			_y = _index->getY();
-
 			if (_y == TOPINDEX_H)
 			{
-				_index++;
-				break;
+				if (_grid[0][_x])
+				{
+					_grid[_y][_x] = 1;
+					_index = _movers.erase(_index);
+					--_numMovers;
+
+					_aggregated[_y][_x] = _aggregated[0][_x] + 1;
+					VERTEXARRAY[(_y * GRID_H) + _x].color = *_aggregated[_y][_x];
+					_anim.push_back({ _x, _y });
+					break;
+				}
+				else
+				{
+					_index->second = 0;
+				}
 			}
-
-			_x = _index->getX();
-
-			if (_grid[_y][_x])
+			else
 			{
-				_index = _movers.erase(_index);
-				_numMovers--;
+				if (_grid[_y + 1][_x])
+				{
+					_grid[_y][_x] = 1;
+					_index = _movers.erase(_index);
+					_numMovers--;
 
-				break;
+					_aggregated[_y][_x] = _aggregated[_y + 1][_x] + 1;
+					VERTEXARRAY[(_y * GRID_H) + _x].color = *_aggregated[_y][_x];
+					_anim.push_back({ _x, _y });
+					break;
+				}
+				else
+				{
+					++(_index->second);
+				}
 			}
 
-			if (_grid[_y + 1][_x])
-			{
-				_grid[_y][_x] = 1;
-				_movers.erase(_index);
-				_numMovers--;
-
-				_targetColor = _aggregated[_y].begin() + _x;
-				_anim[_targetColor->getRegion()].push_back(Point{ _x , _y });
-				*_targetColor = _aggregated[_y + 1][_x];
-				_targetColor->advanceSecondary();
-				erase(_x, _y);
-				SetPixel(_deviceContext, _x, _y, RGB(_targetColor->getR(), _targetColor->getG(), _targetColor->getB()));
-
-				break;
-			}
-
-			_index->upY();
-			_index++;
+			++_index;
 
 			break;
 		}
 		case 3:
 		{
-			_y = _index->getY();
-
-			if (!_y)
+			if (_y == 0)
 			{
-				_index++;
-				break;
+				if (_grid[TOPINDEX_H][_x])
+				{
+					_grid[_y][_x] = 1;
+					_index = _movers.erase(_index);
+					--_numMovers;
+
+					_aggregated[_y][_x] = _aggregated[TOPINDEX_H][_x] + 1;
+					VERTEXARRAY[(_y * GRID_H) + _x].color = *_aggregated[_y][_x];
+					_anim.push_back({ _x, _y });
+					break;
+				}
+				else
+				{
+					_index->second = TOPINDEX_H;
+				}
+			}
+			else
+			{
+				if (_grid[_y - 1][_x])
+				{
+					_grid[_y][_x] = 1;
+					_index = _movers.erase(_index);
+					--_numMovers;
+
+					_aggregated[_y][_x] = _aggregated[_y - 1][_x] + 1;
+					VERTEXARRAY[(_y * GRID_H) + _x].color = *_aggregated[_y][_x];
+					_anim.push_back({ _x, _y });
+					break;
+				}
+				else
+				{
+					--(_index->second);
+				}
 			}
 
-			_x = _index->getX();
-
-			if (_grid[_y][_x])
-			{
-				_index = _movers.erase(_index);
-				_numMovers--;
-
-				break;
-			}
-
-			if (_grid[_y - 1][_x])
-			{
-				_grid[_y][_x] = 1;
-				_movers.erase(_index);
-				_numMovers--;
-
-				_targetColor = _aggregated[_y].begin() + _x;
-				_anim[_targetColor->getRegion()].push_back(Point{ _x , _y });
-				*_targetColor = _aggregated[_y - 1][_x];
-				_targetColor->advanceSecondary();
-				erase(_x, _y);
-				SetPixel(_deviceContext, _x, _y, RGB(_targetColor->getR(), _targetColor->getG(), _targetColor->getB()));
-
-				break;
-			}
-
-			_index->downY();
-			_index++;
+			++_index;
 
 			break;
 		}
 		default:
-			exit(1);
+		{
 			break;
 		}
+		}
 	}
+}
+
+static DLA D1 = DLA{ NUM_MOVERS, NUM_SEEDS };
+static bool RESET = 0;
+
+int main()
+{
+	cout << "Seed: " << SEED << endl;
+	for (unsigned i = 0; i < RANDOM.size(); i++)
+	{
+		RANDOM[i] = (_next() % 4);
+	}
+	for (unsigned i = 0; i < GRID_H; i++)
+	{
+		for (unsigned j = 0; j < GRID_W; j++)
+		{
+			VERTEXARRAY[(i * GRID_H) + j] = sf::Vertex{ sf::Vector2f{ (float)j , (float)i }, sf::Color{ 0 , 0 , 0 } };
+		}
+	}
+
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	static sf::RenderWindow window(sf::VideoMode{ GRID_W , GRID_H }, "DLA", sf::Style::Default, settings);
+
+	sf::Event event;
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+			{
+				window.close();
+				break;
+			}
+			case sf::Event::KeyPressed:
+			{
+				sf::Vector2i position = sf::Mouse::getPosition(window);
+				VERTEXARRAY[(position.y * GRID_H) + position.x].color = sf::Color{ 160 , 60 , 230 };
+				D1.draw(position.x, position.y);
+				break;
+			}
+			default:
+				break;
+			}
+		}
+
+		if (D1.getMovers() < 100)
+		{
+			D1.animateClassic();
+		}
+		else
+		{
+			RANDOM_ITR = RANDOM.begin() - 1;
+			D1.iterateClassic();
+		}
+
+
+		window.draw(VERTEXARRAY);
+		window.display();
+	}
+
+	return 0;
 }
